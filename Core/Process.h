f05961d68527c2fa479a93fa3789cf7e3a99d74d
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-26 19:05:35 +0100

    Add files via upload

---
+++ Core/Process.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Aurora\Types.h>
+#include <Aurora\Core\Signal.h>
+
+namespace Aurora
+{
+	namespace Core
+	{
+		class Process
+		{
+		private:
+			//This process ID
+			U16 ID;
+		public:
+			//Init this process and set the PID to 'ID'
+			Process();
+
+			//Init an external process by name and set it PID to 'ID'
+			Process(const S8*);
+
+			//Init an external process by pid and set it PID to 'ID'
+			Process(const U16);
+
+			//Abort this process, no cleanup
+			Void Abort();
+
+			//Exit this process, cleanup
+			Void Exit();
+		};
+	}
+}
