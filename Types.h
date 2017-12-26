4a47aa01e1d36b3edae73f2cc9f36f034d9d472a
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-26 18:35:50 +0100

    Update Types.h

--- Types.h
+++ Types.h
@@ -9,18 +9,21 @@
 #define UseThreads omp_set_num_threads
 #define MaxThreads omp_get_max_num_threads()
 typedef void Void;
-#define Bit _Bool;
-typedef std::uint_fast8_t UByte;
-typedef std::int_fast8_t Byte;
-typedef std::uint_fast16_t UShort;
-typedef std::int_fast16_t Short;
-typedef std::uint_fast32_t UInt;
-typedef std::int_fast32_t Int;
-typedef std::uint_fast64_t UBig;
-typedef std::int_fast64_t Big;
-typedef float Float;
-typedef double Double;
-typedef long double Quad;
+#define Bit _Bool
+#define Yes 1
+#define True 1
+#define No 0
+#define False 0
+typedef std::uint_fast8_t U8;
+typedef std::int_fast8_t S8;
+typedef std::uint_fast16_t U16;
+typedef std::int_fast16_t S16;
+typedef std::uint_fast32_t U32;
+typedef std::int_fast32_t S32;
+typedef std::uint_fast64_t U64;
+typedef std::int_fast64_t S64;
+typedef float F32;
+typedef double F64;
 
 template<class T, class C>
 C Cast(const T a)
