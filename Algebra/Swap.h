e03699d604d526b76fef9be10e6010ed615e8886
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-27 23:17:09 +0100

    Add files via upload

---
+++ Algebra/Swap.h
@@ -0,0 +1,18 @@
+//Copyright 2018 Samuel Alonso, All Rigths Reserved
+#pragma once
+
+#include <Aurora\Types.h>
+
+namespace Aurora
+{
+	namespace Algebra
+	{
+		template<class T>
+		Void Swap(T& a, T& b)
+		{
+			T c = a;
+			a = b;
+			b = c;
+		}
+	}
+}
