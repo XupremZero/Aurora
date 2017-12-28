f7a6812a3e562f3cb848ed82913dbc8f2fd12730
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-12-28 17:02:36 +0100

    Update Process.h

--- Core/Process.h
+++ Core/Process.h
@@ -13,7 +13,7 @@ namespace Aurora
 			//This process ID
 			U16 ID;
 		public:
-			//Init this process and create, set the PID to 'ID', and create a Window(without borders) with the renderer context
+			//Init this process and create, set the PID to 'ID', and create a Window(without borders, fullscreen) with the renderer context
 			Process();
 
 			//Init an external process by name and set it PID to 'ID'
